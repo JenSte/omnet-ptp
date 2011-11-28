f6a8a1719581893b4854dc5db1ff319d6e1a4b50
Jens Steinhauser <e0826623@student.tuwien.ac.at>
2011-11-28 14:30:10 +0100

    PtpSlave: send Delay_Req packets

--- src/ptpslave.cc
+++ src/ptpslave.cc
@@ -9,17 +9,48 @@ void PtpSlave::initialize()
 	clock = SoftwareClock::findFirstClock(getParentModule());
 }
 
+void PtpSlave::sendDelayReq(const MACAddress& masterMAC)
+{
+	PtpPacket* p = new PtpPacket("ptp delay_req packet", IEEE802CTRL_DATA);
+	p->setType(Ptp::Delay_Req);
+
+	// gets overwritten by the phy when hardware
+	// timestamping is used
+	p->setTtx(clock->getSWtime());
+
+	Ieee802Ctrl* c = new Ieee802Ctrl();
+	c->setDest(masterMAC);
+	c->setEtherType(Ptp::Ethertype);
+	p->setControlInfo(c);
+
+	send(p, "port$o");
+}
+
 void PtpSlave::handleMessage(cMessage* msg)
 {
 	if (PtpPacket* ptp = dynamic_cast<PtpPacket*>(msg)) {
-		// if there is no hardware timestamp by
-		// the phy, use a software timestamp
-		if (0 == ptp->getTrx().raw())
-			ptp->setTrx(clock->getSWtime());
+		switch (ptp->getType()) {
+		case Ptp::Sync:
+			// if there is no hardware timestamp by
+			// the phy, use a software timestamp
+			if (0 == ptp->getTrx().raw())
+				ptp->setTrx(clock->getSWtime());
+
+			// if this is the first sync packet received
+			// from a master clock, use it from now on
+			if (masterMAC.isUnspecified()) {
+				Ieee802Ctrl* c = check_and_cast<Ieee802Ctrl*>(ptp->getControlInfo());
+
+				masterMAC = c->getSrc();
+			}
+
+			sendDelayReq(masterMAC);
+
 
 		EV << "delay: " << (ptp->getTrx() - ptp->getTtx()) << '\n';
 
 
+		}
 	}
 
 	delete msg;
--- src/ptpslave.h
+++ src/ptpslave.h
@@ -2,12 +2,17 @@
 #define PTPSLAVE
 
 #include <omnetpp.h>
+#include <MACAddress.h>
 
 class SoftwareClock;
 
 class PtpSlave: public cSimpleModule
 {
 	SoftwareClock* clock;
+
+	MACAddress masterMAC;
+
+	void sendDelayReq(const MACAddress& masterMAC);
 
 protected:
 	void initialize();
